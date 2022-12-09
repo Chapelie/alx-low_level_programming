3b1664dc966a2f0621d97cd6b37154727a4a0480
Chapelie <clemsobonding@gmail.com>
2022-12-09 06:21:22 -0800

    update

---
+++ 4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+*main - Main function of my project
+*/
+int main(void)
+{
+
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
+	return (0);
+}
