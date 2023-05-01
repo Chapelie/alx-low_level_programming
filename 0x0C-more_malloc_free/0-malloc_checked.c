a981c63b0acf2f22727f11b58a57f0b53aad1194
root <root@debian>
2023-05-01 11:38:27 +0000

    update

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * write a function that allocates memory using malloc
+ *
+ * Returns a pointer to the allocated memory
+ *
+ * 
+ *
+ */
+
+void *malloc_checked(unsigned int b){
+    int *P;
+    P=(int*)malloc(b);
+    return p;
+}
